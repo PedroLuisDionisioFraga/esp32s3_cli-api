98cbcbe1085beed37958fb5595794daf2ba25b01
PedroLuisDionisioFraga <pedrodfraga@hotmail.com>
2026-02-05 18:08:53 -0300

    ♻️ refactor(extern): Remove unnecessary C++ extern blocks for command registration functions

--- components/cmd_wifi/cmd_wifi.h
+++ components/cmd_wifi/cmd_wifi.h
@@ -13,14 +13,5 @@
 */
 #pragma once
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-  // Register WiFi functions
-  void register_wifi(void);
-
-#ifdef __cplusplus
-}
-#endif
+// Register WiFi functions
+void register_wifi(void);
